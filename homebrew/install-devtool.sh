8be9fdc236d0e8b0e90e13e9f787fe25d9c9bcd8
Johnny Da Costa <johnny.dacosta@proton.me>
2024-09-14 18:28:36 +0200

    Init homebrew and script shell to install common devtool

---
+++ homebrew/install-devtool.sh
@@ -0,0 +1,9 @@
+brew install --cask iterm2
+brew install git
+brew install zsh
+brew install neovim
+brew install wget
+brew install --cask brave-browser
+brew install docker
+
+curl -s "https://get.sdkman.io" | bash
