3a8c3107a2f5123862e6788e006faf4d74d1f0c6
Johnny Da Costa <johnny.dacosta@proton.me>
2024-09-29 11:10:46 +0200

    Update brew devtools

--- homebrew/install-devtool.sh
+++ homebrew/install-devtool.sh
@@ -1,11 +1,42 @@
-brew install --cask iterm2
-brew install git
-brew install zsh
-brew install neovim
-brew install wget
-brew install lazygit
-brew install lazydocker 
-brew install --cask brave-browser
-brew install docker
+#!/bin/bash
 
+# List of applications to install with Homebrew Cask
+cask_apps=(
+  iterm2
+  brave-browser
+)
+
+# List of applications to install with Homebrew
+brew_apps=(
+  git
+  zsh
+  neovim
+  wget
+  lazygit
+  lazydocker
+  docker
+  yazi
+  ffmpegthumbnailer
+  sevenzip
+  jq
+  poppler
+  fd
+  ripgrep
+  fzf
+  zoxide
+  imagemagick
+  font-symbols-only-nerd-font
+)
+
+# Install applications using Homebrew Cask
+for app in "${cask_apps[@]}"; do
+  brew install --cask "$app"
+done
+
+# Install applications using Homebrew
+for app in "${brew_apps[@]}"; do
+  brew install "$app"
+done
+
+# Install SDKMAN
 curl -s "https://get.sdkman.io" | bash
